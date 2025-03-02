1fb3f803e5643ddfdc9fe9c4bd3389fe12402523
ernienaylor <enaylor@gmail.com>
2025-03-02 13:52:42 -0600

    Update disable-static-generation.js with clearer instructions

--- src/app/disable-static-generation.js
+++ src/app/disable-static-generation.js
@@ -2,4 +2,8 @@
 // It's imported in the app/layout.tsx file
 
 // Force dynamic rendering for all pages
-export const dynamic = 'force-dynamic'; 
+export const dynamic = 'force-dynamic';
+
+// This file should be imported in the layout.tsx file with:
+// import './disable-static-generation';
+// This will ensure that all pages are dynamically rendered and not statically generated 
