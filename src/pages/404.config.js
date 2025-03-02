ee6270af799e33338674703f4ecf53081b550989
ernienaylor <enaylor@gmail.com>
2025-03-02 13:48:50 -0600

    Remove 404.config.js file that was causing build errors

--- src/pages/404.config.js
+++
@@ -1,7 +0,0 @@
-// This file is used to configure the 404 page
-// It disables static generation for the 404 page
-
-export const config = {
-  unstable_runtimeJS: true,
-  unstable_JsPreload: false,
-}; 
